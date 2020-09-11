7fadc389fad931641800d02757e0836e5ab88af9
Maxime Belcour <maximebelcour@gmail.com>
2020-09-11 11:56:10 +0200

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -4,3 +4,8 @@ FROM voiture;
 
 --Liste des voitures avec toutes les colonnes
 SELECT * FROM voiture;
+
+--Liste triée des noms des marques
+SELECT marques.NomMarque
+FROM marques
+WHERE marques.NomMarque = '';
