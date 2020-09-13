97604639b2101889cb7c8cb53c6d1e20373e1926
Maxime Belcour <maximebelcour@gmail.com>
2020-09-13 16:04:06 +0200

    [FIX] déploiement sur datagrip en accordant les règles de configuration SQL

--- requetes.sql
+++ requetes.sql
@@ -8,30 +8,30 @@ SELECT * FROM voiture;
 --Liste triée des noms des marques
 SELECT marques.NomMarque
 FROM marques
-ORDER BY marques.NomMarque ASC
+ORDER BY marques.NomMarque ASC;
 
 -- produit cartésien de 2 tables
 SELECT marques.IDMarque, marques.NomMarque, voiture.Marque, voiture.Nom
 FROM marques, voiture
-WHERE marques.IDMarque = voiture.Marque
+WHERE marques.IDMarque = voiture.Marque;
 
 -- join (jointure) de 2 tables avec égalité sur les ID
 SELECT marques.IDMarque, marques.NomMarque, voiture.Marque, voiture.Nom
 FROM marques, voiture
-WHERE marques.IDMarque = voiture.Marque
+WHERE marques.IDMarque = voiture.Marque;
 
---Même chose sans l'affichage des IDs
+-- Même chose sans l'affichage des IDs
 SELECT marques.NomMarque, voiture.Nom
 FROM marques, voiture
-WHERE marques.IDMarque = voiture.Marque
+WHERE marques.IDMarque = voiture.Marque;
 
---Liste triée des voiture de plus 300cv
+-- Liste triée des voiture de plus 300cv
 SELECT voiture.Puissance, voiture.Nom
 FROM voiture
 WHERE voiture.Puissance > '300';
-ORDER BY Puissance ASC
+ORDER BY Puissance ASC;
 
---Quelle est la voiture la moins chère ?
+-- Quelle est la voiture la moins chère ?
 SELECT voitures.Nom FROM voiture
 ORDER BY Prix ASC
 Limit 1
